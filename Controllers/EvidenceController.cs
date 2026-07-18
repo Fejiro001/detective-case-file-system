1c0d4dea1e461fd78627f0eefa5f67e117df67b8
Fejiro001 <aberefejiro@gmail.com>
2026-07-18 15:41:34 -0500

    Added static _evidences list and _nextId for storing evidence items in memory. Updated Index action to return evidence list. Implemented [HttpGet] Create action.

--- Controllers/EvidenceController.cs
+++ Controllers/EvidenceController.cs
@@ -1,10 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DetectiveCaseFileSystem.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DetectiveCaseFileSystem.Controllers
 {
     public class EvidenceController : Controller
     {
+        private static List<Evidence> _evidences = new List<Evidence>();
+        private static int _nextId = 1;
         public IActionResult Index()
+        {
+            return View(_evidences);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
         {
             return View();
         }
